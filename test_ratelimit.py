b6f07ff20315d2e8c518bcc7a24f9753f3ef414e
Roberto Polli <robipolli@gmail.com>
2022-05-26 23:14:40 +0200

    test code.

--- test_ratelimit.py
+++ test_ratelimit.py
@@ -17,3 +17,26 @@ def test_policy():
         for i in l:
             assert i.value
             assert "w" in i.params
+            print("value: ", i.value, "params:", dict(i.params))
+
+
+def test_all_in_one():
+    values = [
+        (
+            "limit=10, remaining=10, reset=4,"
+            """policy=(10;w=1 50;w=60 1000;w=3600 5000;w=86400)"""
+        ),
+    ]
+    for v in values:
+        d = http_sfv.Dictionary()
+        d.parse(v.encode())
+        for k, v in d.items():
+            print("parameter:", k, "value:", v)
+            assert k
+            assert v
+            if hasattr(v, "__iter__"):
+                for i in v:
+                    assert i.value
+                    assert "w" in i.params
+                    print("value: ", i.value, "params:", dict(i.params))
+    raise NotImplementedError
