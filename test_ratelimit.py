3937a66fe0b711600c428699061291aa1807c834
Roberto Polli <robipolli@gmail.com>
2022-07-06 14:03:34 +0200

    Preliminary editorial. See #89. (#90)
    * HTTP Style guide.
    * Clarify "too far in the future".
    * Remove tautological requirement
    
    Co-authored-by: Alejandro Martinez Ruiz <alex@flawedcode.org>

--- test_ratelimit.py
+++ test_ratelimit.py
@@ -40,3 +40,87 @@ def test_all_in_one():
                     assert "w" in i.params
                     print("value: ", i.value, "params:", dict(i.params))
     raise NotImplementedError
+
+
+def find_quota_policy(policies, limit):
+    for policy in policies:
+        if policy.value == limit:
+            return policy.params
+    return {}
+
+
+def parse_fields(headers):
+    # 27 µs ± 499 ns per loop (mean ± std. dev. of 7 runs, 10,000 loops each)
+    limit = http_sfv.Item()
+    limit.parse(headers["limit"].encode())
+
+    policies = http_sfv.List()
+    policies.parse(headers["policy"].encode())
+    quota_policy = find_quota_policy(policies, limit.value)
+    return limit, policies, quota_policy
+
+
+def parse_fields_token(headers):
+    # 24.2 µs ± 506 ns per loop (mean ± std. dev. of 7 runs, 10,000 loops each)
+    limit = http_sfv.Item()
+    limit.parse(headers["limit"].encode())
+
+    policies = http_sfv.Dictionary()
+    policies.parse(headers["policy"].encode())
+    quota_policy = policies[f"q{limit.value}"]
+    return limit, policies, quota_policy.params
+
+
+def parse_fields_int(headers):
+    # 24.5 µs ± 297 ns per loop (mean ± std. dev. of 7 runs, 10,000 loops each)
+    limit = int(headers["limit"])
+    policies = http_sfv.List()
+    policies.parse(headers["policy"].encode())
+    quota_policy = find_quota_policy(policies, limit)
+    return limit, policies, quota_policy
+
+
+def test_get_policy():
+    # To retreive the policy, I need to iterate over the list.
+    headers = dict(
+        policy=(
+            """10;w=1,"""
+            """50;w=60,"""
+            """1000;w=3600;comment="foo", """
+            """5000;w=86400"""
+        ),
+        limit="""1000""",
+    )
+
+    limit, policies, quota_policy = parse_fields(headers)
+    assert limit.value == 1000
+
+    for policy in policies:
+        print("parameter:", policy.value, "value:", policy.params["w"])
+
+
+def test_get_policy_tokenized():
+    # To retreive the policy, I need to iterate over the list.
+    headers = dict(
+        policy=(
+            """q10;w=1,"""
+            """q50;w=60,"""
+            """q1000;w=3600;comment="foo", """
+            """q5000;w=86400"""
+        ),
+        limit="""1000""",
+    )
+    limit = http_sfv.Item()
+    limit.parse(headers["limit"].encode())
+    assert limit.value == "q1000"
+
+    policies = http_sfv.List()
+    policies.parse(headers["policy"].encode())
+    for policy in policies:
+        print("parameter:", policy.value, "value:", policy.params["w"])
+
+    # I could create a dictionary out of the list.
+    policies_dict = {policy.value: policy.params for policy in policies}
+    print("policies:", policies_dict)
+    print("current policy:", policies_dict[limit.value])
+    raise NotImplementedError
